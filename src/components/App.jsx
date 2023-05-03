17b34c5e0801c154243fbb29759ac2b57ac9bdeb
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-05-03 22:09:48 +0300

    init libraries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,36 @@
 import { Route, Routes } from 'react-router-dom';
+import Notiflix from 'notiflix';
 
+import { useState } from 'react';
 import Home from 'pages/Home/Home';
-import MovieInfo from 'pages/Home/Home';
-import SearchMovies from 'pages/Home/Home';
+import MovieInfo from 'pages/MovieInfo/MovieInfo';
+import SearchMovies from 'pages/Movies/Movies';
 import { Layout } from './Layout/Layout';
 // import { Cast } from './Cast/Cast';
 // import { Reviews } from './Reviews/Reviews';
 
 export const App = () => {
+  const [searchQuerry, setSearchQuerry] = useState('');
+
+  const handleSubmit = searchWord => {
+    console.log('submit form');
+    if (searchWord === searchQuerry) {
+      return Notiflix.Notify.warning('Try another word to find');
+    }
+    // setSearchQuerry(searchWord.toLowerCase().trim());
+    // setPage(1);
+    // setImages([]);
+    // setShowButton(false);
+    // setIsLoading(true);
+  };
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="movies" element={<SearchMovies />} />
+        <Route
+          path="movies"
+          element={<SearchMovies handlerSubmit={handleSubmit} />}
+        />
         <Route path="movies/:movieId" element={<MovieInfo />}>
           {/* <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} /> */}
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,7 +1,54 @@
 // import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { FaSistrix } from 'react-icons/fa';
+import {
+  SearchBarHeader,
+  SearchForm,
+  SearchInput,
+  SearchFormBtn,
+  SearchFormBtnLabel,
+} from './Movies.styled';
 
-const SearchMovies = () => {
-  return <div>Movies</div>;
+const SearchMovies = ({ handlerSubmit }) => {
+  const [searchQuerry, setSearchQuerry] = useState('');
+
+  const onChange = ({ target }) => {
+    setSearchQuerry(target.value);
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    handlerSubmit(searchQuerry);
+    setSearchQuerry('');
+  };
+
+  return (
+    <SearchBarHeader>
+      <SearchForm onSubmit={onSubmit}>
+        <SearchFormBtn type="submit" className="button">
+          <FaSistrix size="24px" fill="#fff" />
+          <SearchFormBtnLabel className="button-label">
+            Search
+          </SearchFormBtnLabel>
+        </SearchFormBtn>
+
+        <SearchInput
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchQuerry}
+          onChange={onChange}
+        />
+      </SearchForm>
+    </SearchBarHeader>
+  );
+};
+
+SearchMovies.propTypes = {
+  handlerSubmit: PropTypes.func.isRequired,
 };
 
 export default SearchMovies;
